d0bd5015dc61cfe79a13385a19a4cbd84705d6b7
Bradley Gillap <bradgillap@gmail.com>
2017-09-26 16:16:25 -0400

    Fancy js typing.

--- OctoberCMS/papermachine/assets/js/matcustom.js
+++ OctoberCMS/papermachine/assets/js/matcustom.js
@@ -14,7 +14,32 @@ $( document ).ready(function() {
         onClose: function(el) {  }, // A function to be called when sideNav is closed
     });
 
-
-
-
+$('.type-it').typeIt({
+     speed: 70,
+     lifeLike: true,
+     autoStart: true,
+     loop: true
+})
+.tiSettings({speed: 150})
+.tiPause(500)
+.tiDelete(11)
+.tiType(' <span class="red-text darken-1">Javascript</span>.')
+.tiPause(500)
+.tiDelete(11)
+.tiType(' <span class="blue-text lighten-1">Py</span><span class="amber-text lighten-1">thon</span>.')
+.tiPause(500)
+.tiDelete(2)
+.tiType(' <span class="white-text">#</span><span class="light-green-text accent-4">Bash</span>.')
+.tiPause(700)
+.tiDelete(6)
+.tiType(' <span class="blue-text lighten-1">VbScript</span>.')
+.tiPause(500)
+.tiDelete(3)
+.tiType(' <span class="grey-text lighten-1">>Batch</span>.')
+.tiPause(600)
+.tiDelete(0)
+.tiType(' <span class="light-blue-text lighten-2">CSS</span>.')
+.tiPause(600)
+.tiDelete(4)
+.tiSettings({speed: 70})
 });
