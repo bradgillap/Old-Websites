f02cfea8fa2470ecfa90d0d8bb9f0b1e1d63a458
Bradley Gillap <bradgillap@users.noreply.github.com>
2017-09-25 20:48:25 -0400

    Some homepage work

--- OctoberCMS/papermachine/assets/js/matcustom.js
+++ OctoberCMS/papermachine/assets/js/matcustom.js
@@ -1,23 +1,20 @@
  // A $( document ).ready() block.
 $( document ).ready(function() {
 
-
+    $('.parallax').parallax();
  
  // Configuration Options
 
-$('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 300
-    edge: 'left', // Choose the horizontal origin
-    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    draggable: true, // Choose whether you can drag to open on touch screens,
-    onOpen: function(el) { }, // A function to be called when sideNav is opened
-    onClose: function(el) {  }, // A function to be called when sideNav is closed
-    }
-);
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 300
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true, // Choose whether you can drag to open on touch screens,
+        onOpen: function(el) { }, // A function to be called when sideNav is opened
+        onClose: function(el) {  }, // A function to be called when sideNav is closed
+    });
 
-    .parallax-container {
-      height: "500";
-    }
+
 
 
 });
